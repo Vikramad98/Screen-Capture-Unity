146144b0f080c5b5af1b40177ab7b5d550d6d2bb
vikramad98 <gaikwadvikramaditya@gmail.com>
2021-04-23 00:12:31 +0530

    Screen capture for wharfstreetstudios internship

---
+++ Assets/Scripts/ScreenShot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenShot : MonoBehaviour
+{
+    public int count = 0;
+    private void Start()
+    {
+        Debug.Log(Application.persistentDataPath);
+    }
+    public void screenCapture()
+    {
+        count++;
+        StartCoroutine(TakeScreenShot());
+    }
+    public IEnumerator TakeScreenShot()
+    {
+        
+        yield return null;
+        GameObject.Find("Canvas").GetComponent<Canvas>().enabled = false;
+
+        
+        yield return new WaitForEndOfFrame();
+
+        
+        ScreenCapture.CaptureScreenshot(Application.persistentDataPath + "/WSS" +count+".png");
+        Debug.Log("Screenshot Saved!");
+        
+        GameObject.Find("Canvas").GetComponent<Canvas>().enabled = true;
+    }
+}
